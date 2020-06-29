76cce4cd80ec1a26d25638638373cb9d6cf92d62
lijinho <58988274+lijinho@users.noreply.github.com>
2020-06-29 03:00:11 -0400

    some function fix

--- facebook_group_member_scraping.py
+++ facebook_group_member_scraping.py
@@ -22,58 +22,64 @@ driver = webdriver.Chrome(lk, options=chrome_options)
 def hasNumbers(inputString):
   return bool(re.search(r'\d', inputString))
 def get_member(link, group_name):
-  contact_detail = ['','','','','','','','']
-  contact_detail[0] = link
-  contact_detail[1] = group_name
-  driver.get(link)
-  time.sleep(3)
-  member_content = soup(driver.page_source, 'html.parser')
   try:
-    contact_info = member_content.findAll('li',{"class":"_1zw6 _md0 _5h-n _5vb9"})
-    for i in range(len(contact_info)):
-      index = len(contact_info[i].div.i['class'])
-      if(contact_info[i].div.i['class'][index-1] == "sx_1112e7"):
-        contact_detail[3] = contact_info[i].div.find('a').text
-  except:
-    pass
+    contact_detail = ['','','','','','','','']
+    contact_detail[0] = link
+    contact_detail[1] = group_name
+    driver.get(link)
+    time.sleep(3)
+    member_content = soup(driver.page_source, 'html.parser')
+    try:
+      contact_info = member_content.findAll('li',{"class":"_1zw6 _md0 _5h-n _5vb9"})
+      for i in range(len(contact_info)):
+        index = len(contact_info[i].div.i['class'])
+        if(contact_info[i].div.i['class'][index-1] == "sx_1112e7"):
+          contact_detail[3] = contact_info[i].div.find('a').text
+    except:
+      pass
 
-  member_name = member_content.find(id = "fb-timeline-cover-name").text
-  contact_detail[2] = member_name
-  driver.find_element_by_xpath("//ul[contains(@class, '_6_7 clearfix')]/li[2]/a").click()
-  
-  time.sleep(3)
-  member_content = soup(driver.page_source, 'html.parser')
-  total_detail = member_content.find("ul",{"class":"uiList _5yql _4kg"}).findAll("li")
-  for j in range(len(total_detail)):
-    if (total_detail[j].div != None):
-      detail_content = total_detail[j].div.findAll("div")
-      index = len(detail_content)
-      if(detail_content[0].div.i['class'][2] == "sx_896b3a"):
-        contact_detail[6] = detail_content[index-1].text
-      elif(detail_content[0].div.i['class'][2] == "sx_1be44d"):
-        phone_number = detail_content[index-1].text
-      elif(detail_content[0].div.i['class'][2] == "sx_6c0b45"):
-        total_address = detail_content[index-1].findAll("li")
-        if(len(total_address) == 1):
-          if(hasNumbers(total_address[0].text) == True):
-            address_detail = total_address[0].text.split(",")
-            contact_detail[5] = re.search(r'\d+', address_detail[0]).group(0)
-            contact_detail[3] = address_detail[0].replace( contact_detail[5],"")
-            contact_detail[4] = address_detail[1]
+    member_name = member_content.find(id = "fb-timeline-cover-name").text
+    contact_detail[2] = member_name
+    driver.find_element_by_xpath("//ul[contains(@class, '_6_7 clearfix')]/li[2]/a").click()
+    
+    time.sleep(3)
+    member_content = soup(driver.page_source, 'html.parser')
+    time.sleep(2)
+    total_detail = member_content.find("ul",{"class":"uiList _5yql _4kg"}).findAll("li")
+    for j in range(len(total_detail)):
+      if (total_detail[j].div != None):
+        detail_content = total_detail[j].div.findAll("div")
+        index = len(detail_content)
+        if(detail_content[0].div.i['class'][2] == "sx_896b3a"):
+          contact_detail[6] = detail_content[index-1].text
+        elif(detail_content[0].div.i['class'][2] == "sx_1be44d"):
+          phone_number = detail_content[index-1].text
+        elif(detail_content[0].div.i['class'][2] == "sx_6c0b45"):
+          total_address = detail_content[index-1].findAll("li")
+          if(len(total_address) == 1):
+            if(hasNumbers(total_address[0].text) == True):
+              address_detail = total_address[0].text.split(",")
+              try:
+                contact_detail[5] = re.search(r'\d+', address_detail[0]).group(0)
+                contact_detail[3] = address_detail[0].replace( contact_detail[5],"")
+                contact_detail[4] = address_detail[1]
+              except:
+                pass
+            else:
+              contact_detail[3] = total_address[0].text
           else:
             contact_detail[3] = total_address[0].text
-        else:
-          contact_detail[3] = total_address[0].text
-          address_detail = total_address[1].text.split(",")
-          contact_detail[5] = re.search(r'\d+', address_detail[0]).group(0)
-          contact_detail[4] = address_detail[1]
-  contact_detail[7] = "+56 9 8293 7832"
-
+            address_detail = total_address[1].text.split(",")
+            contact_detail[5] = re.search(r'\d+', address_detail[0]).group(0)
+            contact_detail[4] = address_detail[1]
+  except:
+    pass
+  # contact_detail[7] = "+56 9 8293 7832"
+  time.sleep(2)
   print(contact_detail)
   with open('Facebook_group_memeber_scraping.csv', mode='a', encoding="utf8", newline='') as contact_details:
       student_writer = csv.writer(contact_details, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
       student_writer.writerow(contact_detail)
-  
 def main():
   driver.get("https://www.facebook.com/")
 
@@ -86,6 +92,7 @@ def main():
   driver.find_element_by_xpath('//*[@id="pass"]').send_keys(login_pass)
   driver.find_element_by_xpath('//*[@id="loginbutton"]').click()
   
+  driver.get("https://www.facebook.com/groups/1310022065801315/members/")
   # Get scroll height
   last_height = driver.execute_script("return document.body.scrollHeight")
 
@@ -94,7 +101,7 @@ def main():
       driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
       # Wait to load page
-      time.sleep(3)
+      time.sleep(5)
 
       # Calculate new scroll height and compare with last scroll height
       new_height = driver.execute_script("return document.body.scrollHeight")
